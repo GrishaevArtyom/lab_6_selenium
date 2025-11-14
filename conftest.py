33c191e97238d7c5c8ffa70dbd18827a9bbf5981
artemgrishaev <artemail05@gmail.com>
2025-11-14 20:47:08 +0300

    some fixes

--- conftest.py
+++ conftest.py
@@ -9,6 +9,7 @@ from selenium.webdriver.chrome.options import Options
 def browser():
     # Настройка опций Chrome
     chrome_options = Options()
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
